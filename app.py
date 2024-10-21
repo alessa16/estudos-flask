dc19bb09f2584b36cf415a9502b84749761e7729
alessa16 <132908374+alessa16@users.noreply.github.com>
2024-10-21 10:11:23 -0300

    novo

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask 
- 
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1> Olá <br> Mundo! </h1>"
 
-@app.route("/aluno/<nome>")
-def aluno(nome):
-    return f'<h1>{nome}</h1>'
+@app.route("/aluno")
+def aluno():
+    return  render_template('formulario.html')
+
+
+@app.route("/envio", methods=['POST'])
+def envioForms():
+    nombre=request.form['nome']
+    password=request.form['password']
+    if nombre and password == '123'
+
+    return render_template('nome.html', n=nombre, p=password)
